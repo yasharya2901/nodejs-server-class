06862a36c20480be67c39a301106d591b12f4a79
yasharya2901 <yasharya2901@gmail.com>
2024-07-03 09:53:22 +0530

    Express Classes

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const express = require('express');
+
+const app = express();
+
+let courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
+app.get('/', (req, res) => {
+    // res.send('Hello World');
+    res.json(courses);
+});
 
 
-// const p = path.join(__dirname, 'file.txt');
-// console.log(p);
-
-// const pathFile = path.join(__dirname, __filename);
-// fs.copyFile(pathFile, `${__dirname}/newDir2/${__filename}`, (err) => {
-//     if (err) throw err;
-//     console.log('The file has been copied!');
-//   });
-
-// fs.rm(pathFile,(err) => {
-//     if (err) throw err;
-//     console.log('The file has been deleted!');
-// })
-
-console.log(process.argv)
-
-
-
-// const data = fs.readFileSync('file.txt');
-// console.log("" + data);
-
-
-
-// fs.writeFile('file.txt', "\n class is going on",{flag:'a'}, (err) => {
-//     if (err) throw err;
-//     console.log('The file has been saved!');
-// });
-
-// fs.mkdir('newDir', (err)=> {
-//     if (err) throw err;
-//     console.log('The directory has been created!');
-// })
-
-// fs.rename('newDir', 'newDir2', (err) => {
-//     if (err) throw err;
-//     console.log('The file has been renamed!');
-// });
-
-
-// fs.rmdir('newDir', { recursive: true }, (err) => {
-//     if (err) {
-//       throw err;
-//     }
-  
-//     console.log("The directory " + 'newDir' + " is deleted.");
-//   });
+const port = 3000;
+app.listen(port, () => {
+    console.log(`The server is listening on http://localhost:${port}`);
+})
--- test.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-fs.writeFile('log.txt', 'Hello Node', { flag: 'a' }, function(err) {
-  if (err) {
-    return console.error(err);
-  }
-  console.log('Data appended!');
-});
